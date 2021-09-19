c273b1bd6118d2c698bc6b28271268819b189a60
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-09-19 01:04:04 -0400

    Day 2 Part Two working

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -14,7 +14,39 @@ object Day02 extends App {
 
   val filename = s"Day02.txt"
   val bufferedSource = Source.fromFile(filename)
-  val blockSize = 4
+
+  def Intcode2000(pgm: Array[Int]): Array[Int] = {
+    val blockSize = 4
+    var ip = 0
+    var exit: Boolean = false
+    while (ip < pgm.length & !exit) {
+      val instr = pgm.slice(ip,ip+blockSize).toList
+      //print(s"$instr ")
+      instr.head match {
+        case 1 =>
+          // println("Add")
+          pgm(instr(3)) = pgm(instr(1)) + pgm(instr(2))
+        case 2 =>
+          // println("Multiply")
+          pgm(instr(3)) = pgm(instr(1)) * pgm(instr(2))
+        case 99 =>
+          // println("Exit")
+          exit = true
+        case _ =>
+          println("Error")
+          exit = true
+      }
+      ip += blockSize
+    }
+    pgm
+  }
+
+  def test(pgm: Array[Int], noun:Int, verb:Int): Int = {
+    val pgmTmp = pgm.clone()
+    pgmTmp(1) = noun
+    pgmTmp(2) = verb
+    Intcode2000(pgmTmp)(0)
+  }
 
   val lines = bufferedSource
               .getLines
@@ -22,10 +54,12 @@ object Day02 extends App {
                 line.split(',').map(_.toInt).toArray
               }
 
-  // loop over test data.  Test data had multiple rows and real data has only 1
+  // loop over test data.
+  // Test data file had multiple rows but real data has only 1 row
   for(pgm <- lines) {
 
-    var i = 0
+    val clone = pgm.clone
+
     //Once you have a working computer, the first step is to restore the
     // gravity assist program (your puzzle input) to the "1202 program alarm" state
     // it had just before the last computer caught fire. To do this, before running the program,
@@ -33,26 +67,20 @@ object Day02 extends App {
     // What value is left at position 0 after the program halts?
     pgm(1) = 12
     pgm(2) = 2
-    while (i < pgm.length) {
-      val instr = pgm.slice(i,i+blockSize).toList
-      print(s"$instr ")
-      instr.head match {
-        case 1 =>
-          println("Add")
-          pgm(instr(3)) = pgm(instr(1)) + pgm(instr(2))
-        case 2 =>
-          println("Multiply")
-          pgm(instr(3)) = pgm(instr(1)) * pgm(instr(2))
-        case 99 => println("Exit")
-        case _ => println("Error")
-      }
-
-      i += blockSize
-    }
-    var results = pgm.toList
+    val results = Intcode2000(pgm).toList
     println(s"Result\n:${results}")
     println(s"Answer Part One:  ${results(0)}")
     //println(s"\n\n Next Case:")
+
+    val t1 = System.nanoTime
+    for (noun <- 0 to 99)
+      for (verb <- 0 to 99) {
+        val results2 = test(clone.clone(), noun, verb)
+        if (results2 == 19690720) println(s"Found i = $noun, j = $verb.  Answer Part Two: ${100 * noun + verb}")
+        // break out - how?
+      }
+    val duration = (System.nanoTime - t1) / 1e9d
+    println(s"Duration of Part Two: $duration sec")
   }
 
 
