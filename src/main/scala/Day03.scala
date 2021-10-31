ec74f2db7b03961d0cccda17b863a46ebc0bd0c0
Doug Gallagher <8922663+douggal@users.noreply.github.com>
2021-10-31 11:28:07 -0400

    Day 3 Part One thoughts

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -18,28 +18,49 @@ object Day03 extends App {
   val filename = s"Day03.txt"
   val lines = readFile(filename)
 
-  println(lines)
+  //println(lines)
 
   val t1 = System.nanoTime
 
-  val wire1: Vector[String] = lines(0).split(",").toVector
-  val wire2: Vector[String] = lines(1).split(",").toVector
+  val wire1: Vector[String] = lines(0).split(",").map(_.trim).toVector
+  val wire2: Vector[String] = lines(1).split(",").map(_.trim).toVector
   println(s"Wire 1: $wire1")
   println(s"Wire 2 $wire2")
 
   // brute force?  make a list of all the points each wire touches and
   // find coordinates the two lists have in common
-  val w1pts = List()
+  var circuit = List[Tuple2[Int,Int]]()
 
-  //for (p <== wire1)
+  var currLoc = (0,0)
 
+  for (p <- wire1) {
+    print(s"${p.head},${p.tail} : ")
+    p.head match {
+      case 'R' | 'L' =>
+        val inc = if (p.head == 'R') 1 else -1
+        for (x <- 1 to p.tail.toInt) {
+          currLoc = (currLoc._1 + inc, currLoc._2)
+          circuit = currLoc :: circuit
+        }
+      case 'U' | 'D' => {
+        val inc = if (p.head == 'U') 1 else -1
+        for (x <- 1 to p.tail.toInt) {
+          currLoc = (currLoc._1, currLoc._2 + inc)
+          circuit = currLoc :: circuit
+        }
+      }
+      case _ => var i = 0
+    }
+  }
+  println()
+  println(circuit)
 
 
 
   val duration = (System.nanoTime - t1) / 1e9d
 
   val results = List(0)
-  println(s"Answer Part One:  ${results(0)}")
+  println(s"Answer Part One:  ${results.head}")
   println(s"Run time (by the clock) of Part Three: $duration sec")
 
 
